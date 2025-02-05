38b6cfe15d9495d933551e14de87422a3b6c4e14
Júlia Ohara <juliaohara.br@gmail.com>
2025-02-04 23:31:39 -0300

    feat: add toggle feature flag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { FlagProvider } from "@unleash/nextjs/client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <FlagProvider>
         {children}
+      </FlagProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
 import styles from "./page.module.css";
+import { evaluateFlags, flagsClient, getDefinitions } from "@unleash/nextjs";
 
-export default function Home() {
+const getFlag = async () => {
+
+  const definitions = await getDefinitions({
+    fetchOptions: {
+      next: { revalidate: 15 },
+    },
+  });
+
+  const { toggles } = evaluateFlags(definitions);
+
+  const flags = flagsClient(toggles);
+  const isEnabled = flags.isEnabled("toggle-button");
+
+  // console.log('teste' + isEnabled)
+  return isEnabled;
+};
+
+export default async function Home() {
+  const isEnabled = await getFlag();
+
   return (
     <div className={styles.page}>
-      <button className={styles.button}>Iniciar</button>
+      <p>
+        Feature flag is{" "}
+        <strong>
+          <code>{isEnabled ? "ENABLED" : "DISABLED"}</code>
+        </strong>
+      </p>
+      <button className={styles.button}>{isEnabled ? "ENABLED" : "DISABLED"}</button>
     </div>
   );
 }
